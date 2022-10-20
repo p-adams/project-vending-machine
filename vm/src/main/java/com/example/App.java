10993f66d3b5437810b494a32990f17e9e781a14
Paul Adams <ghusiyy@gmail.com>
2022-10-19 22:15:40 -0700

    init Java backend

---
+++ vm/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * Vending Machine Backend
+ * TODO: migrate from node server
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
