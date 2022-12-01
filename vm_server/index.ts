8e4264d7ef6063572ecd46f3e43f4cc4aefdd62c
Paul Adams <ghusiyy@gmail.com>
2022-12-01 00:40:38 -0800

    socket channel class -> object

--- vm_server/index.ts
+++ vm_server/index.ts
@@ -3,7 +3,10 @@ import fs from "fs";
 const app = express();
 const PORT = 8080;
 import { WebSocketServer, WebSocket } from "ws";
-import SocketChannel from "./socket-channel";
+import {
+  socketChannel as socketChannelCoordinator,
+  SocketChannel,
+} from "./socket-channel";
 
 const wss = new WebSocketServer({ noServer: true });
 
@@ -14,33 +17,28 @@ wss.on("connection", (ws: WebSocket) => {
       case "fetch_inventory":
         const rStock = fs.readFileSync("stock.json");
         const inventory = JSON.parse(rStock.toLocaleString());
-        socketChannel.ws.send(
-          mToStr({
-            channel: "inventory",
-            type: "receive_inventory",
-            inventory,
-          })
-        );
+        socketChannel.dispatch({
+          channel: "inventory",
+          type: "receive_inventory",
+          data: inventory,
+        });
+
         break;
       case "process_payment":
         // mock async process
         setTimeout(() => {
-          socketChannel.ws.send(
-            mToStr({
-              channel: "payment",
-              type: "payment_complete",
-            })
-          );
+          socketChannel.dispatch({
+            channel: "payment",
+            type: "payment_complete",
+          });
         }, 500);
         break;
       default:
         break;
     }
   }
-  const socketChannel = new SocketChannel(ws);
-  /**
-   * dataObj: {channel: "inventory | vm", type: string, data: string | number | object}
-   */
+  const socketChannel = socketChannelCoordinator.init(ws);
+
   socketChannel.receive("message", (data: string) =>
     messageHandler(socketChannel, data)
   );
@@ -54,10 +52,4 @@ server.on("upgrade", (req, ws, h) => {
   });
 });
 
-export function mToStr(message: {
-  channel: string;
-  type: string;
-  inventory?: any;
-}) {
-  return JSON.stringify(message);
-}
+export default {};
--- vm_server/socket-channel.ts
+++ vm_server/socket-channel.ts
@@ -1,19 +1,35 @@
 import { WebSocket } from "ws";
-export default class SocketChannel {
-  #ws: WebSocket;
-  constructor(ws: WebSocket) {
-    this.#ws = ws;
-  }
 
-  public get ws(): WebSocket {
-    return this.#ws;
-  }
-
-  public receive(event = "message", handler: (d: string) => void) {
-    this.ws.on(event, (data: string) => handler(data));
-  }
-
-  public dispatch(data: any) {
-    this.ws.send(data);
-  }
+function mToStr(message: { channel: string; type: string; inventory?: any }) {
+  return JSON.stringify(message);
 }
+
+interface Dispatch {
+  channel: string;
+  type: string;
+  data?: any;
+}
+
+export interface SocketChannel {
+  ws: WebSocket | null;
+  init: (ws: WebSocket) => SocketChannel;
+  receive: (event: string, handler: (d: string) => void) => void;
+  dispatch: (data: Dispatch) => void;
+}
+
+const SocketChannel: SocketChannel = {
+  init: function (ws: WebSocket) {
+    this.ws = ws;
+    return this;
+  },
+  receive: function (event = "message", handler: (d: string) => void) {
+    this.ws?.on(event, (data: string) => handler(data));
+  },
+  dispatch: function (data: Dispatch) {
+    const processedData = mToStr(data);
+    this.ws?.send(processedData);
+  },
+  ws: null,
+};
+
+export const socketChannel: SocketChannel = Object.create(SocketChannel);
