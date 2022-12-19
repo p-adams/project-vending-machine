77706ae8adcb99f9d01591b15598fb301deebaac
Paul Adams <ghusiyy@gmail.com>
2022-12-19 13:32:14 -0800

    add create table skeleton code

--- vm_server/db/index.ts
+++ vm_server/db/index.ts
@@ -4,8 +4,26 @@ const db = new sqlite3.Database(":memory:", async (error) => {
   if (error) {
     return console.error(error.message);
   }
+  console.log("Connected to DB");
+});
+
+db.serialize(async () => {
+  // initialize inventory table if it doesn't already exist
+  // TODO: define table columns
+  const CREATE_INVENTORY_TABLE = "CREATE TABLE IF NOT EXISTS inventory ()";
+  db.run(CREATE_INVENTORY_TABLE, (err) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+  });
   // seed database with inventory json data
   const inventory = await (await import("./stock.json")).default;
-  console.log("Connected to DB", inventory);
+  for (const inventoryItemRow of inventory) {
+    for (const inventoryItem of inventoryItemRow) {
+      console.log(inventoryItem);
+    }
+  }
 });
+
 export default db;
