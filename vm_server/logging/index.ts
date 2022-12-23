2ef3b835ee210ade2c27df60bf224326d64d79ad
Paul Adams <ghusiyy@gmail.com>
2022-12-22 19:58:41 -0800

    add DB logger

--- vm_server/logging/index.ts
+++ vm_server/logging/index.ts
@@ -1,1 +1,28 @@
-// TODO: implement server logging
+import fs from "fs";
+import path from "path";
+
+class Logger {
+  constructor(private path: string) {
+    this.path = path;
+  }
+  write(data: string) {
+    fs.writeFileSync(path.join(__dirname, this.path), data);
+  }
+  read() {
+    fs.readFileSync(this.path);
+  }
+}
+
+class DBLogger extends Logger {
+  constructor(path: string) {
+    super(path);
+  }
+  write(data: string): void {
+    super.write(data);
+  }
+  read(): void {
+    super.read();
+  }
+}
+
+export const dBLogger = new DBLogger("./logs/db.log");
